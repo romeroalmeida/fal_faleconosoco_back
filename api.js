1e90a5e95138f835fabe01f18f132ee6e27f449f
Romero Almeida <romerobm@gmail.com>
2020-10-19 09:18:22 -0300

    UP

---
+++ api.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const upload = require("multer")();
+
+app.use(require("cors")());
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "Tudo ok por aqui!" });
+});
+
+app.post("/send", upload.single("anexo"), (req, res, next) => {
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const telefone = req.body.telefone;
+  const mensagem = req.body.mensagem;
+  require("./nodemail")(email, nome, telefone, mensagem)
+    .then((response) => res.json(response))
+    .catch((error) => res.json(error));
+});
+
+const server = http.createServer(app);
+server.listen(3030);
+console.log("Servidor escutando na porta 3030...");
